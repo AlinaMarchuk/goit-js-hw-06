9f4c4e21cf00895bcdd7687ef9b2a2371a9e3c29
AlinaMarchuk <123773400+AlinaMarchuk@users.noreply.github.com>
2023-03-18 16:01:56 +0200

    task 10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,42 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector('input');
+const divBoxes = document.querySelector('#boxes');
+const btnCreate = document.querySelector('[data-create]');
+const btnDestroy = document.querySelector('[data-destroy]');
+
+let number = 0;
+
+const handleInput = event => {
+  number = event.currentTarget.value;
+};
+
+const handleDestroy = () => {
+  divBoxes.innerHTML = '';
+  input.value = '';
+};
+
+const createBoxes = amount => {
+  const arrBoxes = [];
+  let width = 30;
+  let height = 30;
+
+  for (let i = 0; i < amount; i += 1) {
+    const div = document.createElement('div');
+    div.style.width = `${width}px`;
+    div.style.height = `${height}px`;
+    div.style.backgroundColor = getRandomHexColor();
+    arrBoxes.push(div);
+    width += 10;
+    height += 10;
+  }
+  divBoxes.append(...arrBoxes);
+};
+
+input.addEventListener('input', handleInput);
+btnCreate.addEventListener('click', () => {
+  createBoxes(number);
+});
+btnDestroy.addEventListener('click', handleDestroy);
